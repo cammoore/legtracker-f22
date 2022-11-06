7058b6c4842dfce36357a67f710182d3053ea100
Cam Moore <cammymoore07@gmail.com>
2022-11-06 10:16:52 -1000

    Update ManageDatabase.jsx

--- app/imports/ui/pages/ManageDatabase.jsx
+++ app/imports/ui/pages/ManageDatabase.jsx
@@ -3,13 +3,11 @@ import { Container } from 'react-bootstrap';
 import ManageHearings from '../components/ManageHearings';
 import ManageMeasures from '../components/ManageMeasures';
 
-const ManageDatabase = () => {
-  return (
-    <Container>
-      <ManageHearings />
-      <ManageMeasures />
-    </Container>
-  );
-};
+const ManageDatabase = () => (
+  <Container>
+    <ManageHearings />
+    <ManageMeasures />
+  </Container>
+);
 
 export default ManageDatabase;
